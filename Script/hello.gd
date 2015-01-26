e5cda6389427fb7ff1de41cd28ad2aed28b733fd
Quemael <fjngame@gmail.com>
2015-01-26 16:23:10 -0500

    Added pong
    Added a Godot project called pong

--- Script/hello.gd
+++ Script/hello.gd
@@ -5,8 +5,17 @@ extends Panel
 # var a=2
 # var b="textvar"
 
+var String1 = "I'm a lable to display word by word"
+var n = 1
+var curStr = ""
+
 func _on_button_pressed():
-    get_node("Label").set_text("HELLO!")
+	
+	if(n <= String1.length()): 
+		curStr = String1.left(n)
+		get_node("Label").set_text(curStr)
+		n += 1
+		
 
 func _ready():
 	get_node("Button").connect("pressed",self,"_on_button_pressed")
